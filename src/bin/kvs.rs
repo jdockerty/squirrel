fd7c4ae055b2df4fa9a34c6c1bd4e1c686e8f4f3
Jack Dockerty <jdockerty19@gmail.com>
2023-12-27 21:50:06 +0000

    project 2: wip for wal writing

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,10 +1,22 @@
+use std::path::PathBuf;
+
 use clap::{Parser, Subcommand};
+use kvs::Result;
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 struct App {
+    #[arg(short, long, global = true, default_value = "~/.kvs_log", default_value = default_log_location().into_os_string())]
+    log_file: PathBuf,
+
     #[clap(subcommand)]
     subcmd: Option<Commands>,
+}
+
+fn default_log_location() -> PathBuf {
+    dirs::home_dir()
+        .expect("unable to find home directory")
+        .join(".kvs_log")
 }
 
 #[derive(Debug, Subcommand)]
@@ -23,23 +35,26 @@ enum Commands {
     },
 }
 
-fn main() {
+fn main() -> Result<()> {
     let cli = App::parse();
 
     let mut kv = kvs::KvStore::new();
+    kv.log_location = cli.log_file;
 
     match cli.subcmd {
         Some(Commands::Set { key, value }) => {
-            kv.set(key, value).unwrap();
+            kv.set(key, value)?;
         }
         Some(Commands::Get { key }) => {
-            kv.get(key).unwrap();
+            kv.get(key)?;
         }
         Some(Commands::Remove { key }) => {
-            kv.remove(key).unwrap();
+            kv.remove(key)?;
         }
         None => {
             std::process::exit(1);
         }
     }
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,40 @@
-use std::collections::HashMap;
-use std::path::PathBuf;
-
-#[derive(Debug)]
-pub enum KvStoreError {
-    KeyNotFound,
-    IoError(std::io::Error),
-}
-
-impl std::error::Error for KvStoreError {
-    fn cause(&self) -> Option<&dyn std::error::Error> {
-        match *self {
-            KvStoreError::KeyNotFound => None,
-            KvStoreError::IoError(ref err) => Some(err),
-        }
-    }
-}
-
-impl std::fmt::Display for KvStoreError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match *self {
-            KvStoreError::KeyNotFound => write!(f, "Key not found"),
-            KvStoreError::IoError(ref err) => err.fmt(f),
-        }
-    }
-}
+use bson::Bson;
+use serde::{Deserialize, Serialize};
+use std::{io::Write, path::PathBuf};
+use thiserror::Error;
 
 pub type Result<T> = std::result::Result<T, KvStoreError>;
 
+#[derive(Debug, Error)]
+pub enum KvStoreError {
+    #[error("I/O error: {0}")]
+    IoError(#[from] std::io::Error),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum Operation {
+    Set,
+    Get,
+    Remove,
+}
+
 /// An in-memory key-value store, backed by a [`HashMap`] from the standard library.
 pub struct KvStore {
-    store: HashMap<String, String>,
+    pub log_location: PathBuf,
+}
+
+/// A ['LogEntry'] is a single line entry in the log file.
+/// It can be used to rebuild the state of the store.
+#[derive(Debug, Serialize, Deserialize)]
+struct LogEntry {
+    /// The operation that was performed.
+    operation: Operation,
+    key: String,
+    value: String,
+
+    /// Byte offset of the entry in the log file, this is used for lookups in
+    /// the future.
+    offset: u64,
 }
 
 impl Default for KvStore {
@@ -41,32 +46,50 @@ impl Default for KvStore {
 impl KvStore {
     pub fn new() -> KvStore {
         KvStore {
-            store: HashMap::new(),
+            log_location: PathBuf::from(""),
         }
     }
 
-    pub fn open<F>(file: F) -> Result<KvStore>
+    pub fn open<P>(path: P) -> Result<KvStore>
     where
-        F: Into<PathBuf>,
+        P: Into<PathBuf>,
     {
-        todo!()
+        let mut store = KvStore::new();
+        store.log_location = path.into();
+        Ok(store)
     }
 
     /// Set the value of a key by inserting the value into the store for the given key.
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
-        self.store.insert(key, value);
+        let mut log_file = std::fs::OpenOptions::new()
+            .read(true)
+            .append(true)
+            .create(true)
+            .open(self.log_location.as_path())?;
+
+        let current_offset = log_file.metadata()?.len();
+
+        let entry = LogEntry {
+            operation: Operation::Set,
+            key,
+            value,
+            offset: current_offset,
+        };
+
+        let d = bson::to_document(&entry).unwrap();
+        d.to_writer(&mut log_file).unwrap();
+
         Ok(())
     }
 
     /// Retrieve the value of a key from the store.
     /// If the key does not exist, then [`None`] is returned.
     pub fn get(&self, key: String) -> Result<Option<String>> {
-        Ok(self.store.get(&key).map(|val| val.to_string()))
+        Ok(None)
     }
 
     /// Remove a key from the store.
     pub fn remove(&mut self, key: String) -> Result<()> {
-        self.store.remove(&key);
         Ok(())
     }
 }
