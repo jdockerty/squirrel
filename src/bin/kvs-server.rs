78f4ea0ccca2b96c36a1fb6b9f15bc8499951808
Jack Dockerty <jdockerty19@gmail.com>
2024-02-17 13:47:34 +0000

    fix: implement action result onto server
    When I initially scaffolded this quickly, the actions were on the client which was not quite right. The actions are taken by the client, but they are conducted by the server which maintains the kvstore.

--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -1,14 +1,35 @@
 use clap::Parser;
-use std::net::{SocketAddr, TcpListener};
+use env_logger;
+use kvs::client::Action;
+use kvs::KvStore;
+use log::{debug, error, info};
+use std::io::Write;
+use std::{ffi::OsString, path::PathBuf};
+use std::{
+    fmt::Display,
+    net::{SocketAddr, TcpListener},
+};
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 struct App {
-    #[clap(short, long, default_value = "127.0.0.1:4000")]
+    #[clap(long, default_value = "127.0.0.1:4000")]
     addr: SocketAddr,
 
     #[clap(name = "engine", short, long, default_value = "kvs")]
     engine_name: Engine,
+
+    #[clap(long, default_value = "info")]
+    log_level: log::LevelFilter,
+
+    #[arg(long, global = true, default_value = default_log_location())]
+    log_file: PathBuf,
+}
+
+fn default_log_location() -> OsString {
+    std::env::current_dir()
+        .expect("unable to find current directory")
+        .into_os_string()
 }
 
 #[derive(Debug, Clone, clap::ValueEnum)]
@@ -17,13 +38,69 @@ enum Engine {
     Sled,
 }
 
+impl Display for Engine {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Kvs => write!(f, "kvs"),
+            Self::Sled => write!(f, "sled"),
+        }
+    }
+}
+
 fn main() -> anyhow::Result<()> {
     let app = App::parse();
+    let mut kv = KvStore::open(app.log_file)?;
+    env_logger::builder().filter_level(app.log_level).init();
+
+    info!(
+        "kvs-server version: {}, engine: {}",
+        env!("CARGO_PKG_VERSION"),
+        app.engine_name
+    );
 
     let listener = TcpListener::bind(app.addr)?;
+    info!("listening on {}", app.addr);
 
-    while let Ok((stream, remote_peer)) = listener.accept() {
-        println!("Connection established");
+    while let Ok((mut stream, _)) = listener.accept() {
+        debug!("Connection established: {stream:?}");
+
+        // We know the actions that a client can take, so we can encode and decode them
+        // to know what action we should take.
+        let action: Action = bincode::deserialize_from(&stream)?;
+
+        match &action {
+            Action::Set { key, value } => {
+                debug!("Received action: {action:?}");
+                match kv.set(key.to_string(), value.to_string()) {
+                    Ok(_) => write!(stream, "OK")?,
+                    Err(e) => error!("{}", e),
+                }
+            }
+            Action::Get { key } => {
+                debug!("Received action: {action:?}");
+                match kv.get(key.to_string()) {
+                    Ok(Some(value)) => {
+                        debug!("{key} has value: {value}");
+                        bincode::serialize_into(stream, &value)?;
+                    }
+                    Ok(None) => {
+                        debug!("{key} not found");
+                        write!(stream, "Key not found")?;
+                    }
+                    Err(e) => error!("{}", e),
+                }
+            }
+            Action::Remove { key } => {
+                debug!("Received action: {action:?}");
+                match kv.remove(key.to_string()) {
+                    Ok(_) => {
+                        debug!("{key} removed");
+                        write!(stream, "OK")?;
+                    }
+                    Err(e) => error!("{}", e),
+                }
+            }
+        }
     }
 
     Ok(())
