6737902daff0ec09cea13239450342104ada1bc2
Jack Dockerty <jdockerty19@gmail.com>
2024-02-18 20:36:38 +0000

    docs: update comments

--- src/client.rs
+++ src/client.rs
@@ -2,15 +2,19 @@ use clap::Subcommand;
 use serde::{Deserialize, Serialize};
 
 /// Actions that can be performed by the client.
+///
+/// Subsequently, this means we can also serialize these actions to send them over
+/// the network to the server, which can then deserialize them and perform the
+/// corresponding action.
 #[derive(Debug, Subcommand, Serialize, Deserialize)]
 pub enum Action {
     /// Enter a key-value pair into the store.
     Set { key: String, value: String },
 
-    /// Get a value from the store, providing a key.
+    /// Get a value from the store with the provided key.
     Get { key: String },
 
-    /// Remove a value from the store, providing a key.
+    /// Remove a value from the store with the provided key.
     #[clap(name = "rm")]
     Remove { key: String },
 }
