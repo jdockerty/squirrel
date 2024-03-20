03ca33958766d17eb4fee839dfc81026a4b577ff
Jack Dockerty <jdockerty@influxdata.com>
2024-03-20 17:11:43 +0000

    revert: benchmark test

--- benches/kvs_bench.rs
+++ benches/kvs_bench.rs
@@ -5,16 +5,11 @@ use kvs::{KvStore, KvsEngine};
 fn write_direct(c: &mut criterion::Criterion) {
     let store = KvStore::open(std::env::temp_dir()).expect("Open temp dir for KvStore");
     c.bench_function("write", move |b| {
-        let mut counter = 0;
+        let key = "key".to_string();
+        let value = "value".to_string();
         b.iter(|| {
-            store
-                .set("key".to_string(), format!("{}", counter))
-                .unwrap();
-            assert_eq!(
-                store.get("key".to_string()).unwrap(),
-                Some(format!("{}", counter))
-            );
-            counter += 1;
+            store.set(key.clone(), value.clone()).unwrap();
+            assert_eq!(store.get(key.clone()).unwrap(), Some(value.clone()));
         })
     });
 }
