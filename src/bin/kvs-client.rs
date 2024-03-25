46012197bf6db9403563e22055488440f56f398f
Jack Dockerty <jdockerty@influxdata.com>
2024-03-25 12:29:41 +0000

    feat: introduce tokio to kvs-client

--- src/bin/kvs-client.rs
+++ src/bin/kvs-client.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use kvs::client::Action;
-use std::{io::prelude::*, net::TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
@@ -12,25 +13,29 @@ struct App {
     subcmd: Action,
 }
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     let cli = App::parse();
     let mut response = String::new();
 
     match cli.subcmd {
         Action::Set { key, value } => {
-            let mut stream = TcpStream::connect(cli.server)?;
-            bincode::serialize_into(&mut stream, &Action::Set { key, value })?;
+            let mut stream = TcpStream::connect(cli.server).await?;
+            let data = bincode::serialize(&Action::Set { key, value })?;
+            stream.write_all(&data).await?;
         }
         Action::Get { key } => {
-            let mut stream = TcpStream::connect(cli.server)?;
-            bincode::serialize_into(&mut stream, &Action::Get { key })?;
-            stream.read_to_string(&mut response)?;
+            let mut stream = TcpStream::connect(cli.server).await?;
+            let data = bincode::serialize(&Action::Get { key })?;
+            stream.write_all(&data).await?;
+            stream.read_to_string(&mut response).await?;
             println!("{}", response);
         }
         Action::Remove { key } => {
-            let mut stream = TcpStream::connect(cli.server)?;
-            bincode::serialize_into(&mut stream, &Action::Remove { key })?;
-            stream.read_to_string(&mut response)?;
+            let mut stream = TcpStream::connect(cli.server).await?;
+            let data = bincode::serialize(&Action::Remove { key })?;
+            stream.write_all(&data).await?;
+            stream.read_to_string(&mut response).await?;
             if response.as_str() == "Key not found" {
                 eprintln!("{}", response);
                 std::process::exit(1);
