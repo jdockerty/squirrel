f5695f69344043a0171a84bcaf780a9006740551
Jack Dockerty <jdockerty19@gmail.com>
2024-03-26 18:26:44 +0000

    feat: add serialize_and_hint macro

--- src/bin/kvs-client.rs
+++ src/bin/kvs-client.rs
@@ -13,6 +13,18 @@ struct App {
     subcmd: Action,
 }
 
+/// Serialize the provided action and provided a size hint to the server on
+/// the provided [`TcpStream`].
+///
+/// [`TcpStream`]: tokio::net::TcpStream
+macro_rules! serialize_and_hint {
+    ($stream:expr, $action:expr) => {
+        let data = bincode::serialize(&$action)?;
+        $stream.write_u64(data.len() as u64).await?;
+        $stream.write_all(&data).await?;
+    };
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let cli = App::parse();
@@ -21,15 +33,11 @@ async fn main() -> anyhow::Result<()> {
     match cli.subcmd {
         Action::Set { key, value } => {
             let mut stream = TcpStream::connect(cli.server).await?;
-            let data = bincode::serialize(&Action::Set { key, value })?;
-            stream.write_u64(data.len() as u64).await?;
-            stream.write_all(&data).await?;
+            serialize_and_hint!(stream, Action::Set { key, value });
         }
         Action::Get { key } => {
             let mut stream = TcpStream::connect(cli.server).await?;
-            let data = bincode::serialize(&Action::Get { key })?;
-            stream.write_u64(data.len() as u64).await?;
-            stream.write_all(&data).await?;
+            serialize_and_hint!(stream, Action::Get { key });
             stream.read_to_string(&mut response).await?;
             match response.as_str() {
                 "Key not found" => {
@@ -43,9 +51,7 @@ async fn main() -> anyhow::Result<()> {
         }
         Action::Remove { key } => {
             let mut stream = TcpStream::connect(cli.server).await?;
-            let data = bincode::serialize(&Action::Remove { key })?;
-            stream.write_u64(data.len() as u64).await?;
-            stream.write_all(&data).await?;
+            serialize_and_hint!(stream, Action::Remove { key });
             stream.read_to_string(&mut response).await?;
             if response.as_str() == "Key not found" {
                 eprintln!("{}", response);
