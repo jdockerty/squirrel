b02ae01b40a74bfadf86dcf345c0cb54b9573385
Jack Dockerty <jdockerty19@gmail.com>
2024-04-10 19:47:37 +0100

    chore: basic file structure/test

--- tests/replicated_store.rs
+++ tests/replicated_store.rs
@@ -1,2 +1,12 @@
-#[tokio::test]
-async fn it_works() {}
+//#[tokio::test]
+//async fn general_replication() {
+//    let nodes = vec![SqrlNode{addr: "127.0.0.1:5000"}, SqrlNode{addr:"127.0.0.1:5001"}];
+//    let client = sqrl::client::Client::new("127.0.0.1:5000");
+//
+//    client.set("foo", "foo").await.unwrap();
+//    assert_eq!(client.get("foo").await.unwrap(), Some("foo"));
+//
+//    let client = sqrl::client::Client::new("127.0.0.1:5001");
+//    assert_eq!(client.get("foo").await.unwrap(), Some("foo"));
+//
+//}
