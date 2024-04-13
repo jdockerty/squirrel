e26f9caaffc182444ace02f60c87f28b91b5ce57
Jack Dockerty <jdockerty19@gmail.com>
2024-04-13 20:44:20 +0100

    wip: replication

--- src/replication/client.rs
+++ src/replication/client.rs
@@ -1,17 +1,34 @@
+use std::net::SocketAddr;
+use std::path::PathBuf;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
 use crate::client::Client;
-use crate::proto::Acknowledgement;
-use crate::{KvStore, KvsEngine};
+use crate::proto::{Acknowledgement, GetResponse};
+use crate::{KvServer, KvsEngine};
 
 /// Implementation of a [`Client`] with an awareness of multiple remote cache
 /// servers. These servers act as replication points and will be used to serve
 /// requests based on a quorum read/write sequence.
-#[derive(Clone, Debug)]
-struct ReplicationClient<C> {
-    local_store: Arc<Mutex<KvStore>>,
+#[derive(Clone)]
+pub struct ReplicationClient<C> {
+    local_store: Arc<Mutex<KvServer>>,
     remote_replicas: Arc<Mutex<Vec<C>>>,
+}
+
+impl<C> ReplicationClient<C>
+where
+    C: Client,
+{
+    pub fn new<P>(clients: Vec<C>, path: P, addr: SocketAddr) -> anyhow::Result<Self>
+    where
+        P: Into<PathBuf>,
+    {
+        Ok(Self {
+            local_store: Arc::new(Mutex::new(KvServer::new(path, addr)?)),
+            remote_replicas: Arc::new(Mutex::new(clients)),
+        })
+    }
 }
 
 #[tonic::async_trait]
@@ -19,21 +36,32 @@ impl<C> Client for ReplicationClient<C>
 where
     C: Client + Send + Sync,
 {
-    async fn get(&mut self, key: String) -> anyhow::Result<Option<String>> {
-        if let Some((local_value, local_timestamp)) = self
-            .local_store
-            .lock()
-            .await
-            .get_with_ts(key.clone())
-            .await?
-        {
-            for r in self.remote_replicas.lock().await.iter_mut() {
-                let remote_value = r.get(key.clone()).await?;
+    async fn get(&mut self, key: String) -> anyhow::Result<Option<GetResponse>> {
+        let guard = self.local_store.lock().await;
+        match guard.store.get(key.clone()).await? {
+            Some(local_result) => {
+                let mut quorum_results = vec![];
+                for r in self.remote_replicas.lock().await.iter_mut() {
+                    match r.get(key.clone()).await? {
+                        Some(remote_result) => {
+                            if local_result.timestamp >= remote_result.timestamp {
+                                r.set(key.clone(), local_result.value.clone().unwrap())
+                                    .await?;
+                            } else {
+                                guard
+                                    .store
+                                    .set(key.clone(), remote_result.value.clone().unwrap())
+                                    .await?;
+                                quorum_results.push(remote_result.value);
+                            }
+                        }
+                        None => {}
+                    }
+                }
             }
-            Ok(None)
-        } else {
-            Ok(None)
+            None => {}
         }
+        Ok(None)
     }
 
     async fn set(&mut self, key: String, value: String) -> anyhow::Result<Acknowledgement> {
--- tests/replicated_store.rs
+++ tests/replicated_store.rs
@@ -1,12 +1,36 @@
+//use std::{thread, time::Duration};
+//
+//use sqrl::client::{self, Client, RemoteNodeClient, ReplicationClient};
+//use tempfile::TempDir;
+//
+//async fn client_one() -> RemoteNodeClient {
+//RemoteNodeClient::new("127.0.0.1:6000".to_string()).await.unwrap()
+//}
+//
+//async fn client_two() -> RemoteNodeClient {
+//RemoteNodeClient::new("127.0.0.1:6001".to_string()).await.unwrap()
+//}
+//
 //#[tokio::test]
 //async fn general_replication() {
-//    let nodes = vec![SqrlNode{addr: "127.0.0.1:5000"}, SqrlNode{addr:"127.0.0.1:5001"}];
-//    let client = sqrl::client::Client::new("127.0.0.1:5000");
 //
-//    client.set("foo", "foo").await.unwrap();
-//    assert_eq!(client.get("foo").await.unwrap(), Some("foo"));
+//    let node_one = ReplicationClient::new(
+//        vec![RemoteNodeClient::new("127.0.0.1:6001".to_string()).await.unwrap()],
+//        TempDir::new().unwrap().into_path(),
+//        "127.0.0.1:6000".parse().unwrap(),
+//    );
 //
-//    let client = sqrl::client::Client::new("127.0.0.1:5001");
-//    assert_eq!(client.get("foo").await.unwrap(), Some("foo"));
+//    let node_one = ReplicationClient::new(
+//        vec![RemoteNodeClient::new("127.0.0.1:6000".to_string()).await.unwrap()],
+//        TempDir::new().unwrap().into_path(),
+//        "127.0.0.1:6001".parse().unwrap(),
+//    );
 //
+//    thread::sleep(Duration::from_secs(1));
+//
+//    let mut client = client_one().await;
+//    client.set("key1".to_string(), "value1".to_string()).await.unwrap();
+//
+//    let mut client = client_two().await;
+//    assert_eq!(client.get("key1".to_string()).await.unwrap().unwrap().value, Some("value1".to_string()));
 //}
