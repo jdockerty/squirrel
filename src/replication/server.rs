a4445103ee06161f41035faf6cc72b7cb4d8c283
Jack Dockerty <jdockerty19@gmail.com>
2024-04-14 22:52:33 +0100

    wip: replication
    Simplify to 3 nodes max for logic and try impl with futures Either/select functionality

--- src/replication/server.rs
+++ src/replication/server.rs
@@ -1,7 +1,7 @@
+use futures::future::Either;
 use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::sync::Arc;
-use tokio::sync::Mutex;
 use tracing::info;
 
 use crate::client::Client;
@@ -18,24 +18,30 @@ pub struct ReplicatedServer<C> {
     ///
     /// This is used for troubleshooting/debugging purposes.
     name: String,
-    local_store: Arc<Mutex<StandaloneServer>>,
+    local_store: Arc<StandaloneServer>,
     addr: SocketAddr,
-    remote_replicas: Arc<Mutex<Vec<C>>>,
+
+    /// [`Client`] implementations which provide access to remote replicas.
+    ///
+    /// # Notes
+    /// This is locked at 2 replicas for simplicitiy in handling the replication
+    /// logic of the stores.
+    remote_replicas: Arc<[C; 2]>,
 }
 
 impl<C> ReplicatedServer<C>
 where
     C: Client + Clone + Send + Sync + 'static,
 {
-    pub fn new<P>(clients: Vec<C>, name: String, path: P, addr: SocketAddr) -> anyhow::Result<Self>
+    pub fn new<P>(clients: [C; 2], name: String, path: P, addr: SocketAddr) -> anyhow::Result<Self>
     where
         P: Into<PathBuf>,
     {
         Ok(Self {
             name,
             addr,
-            local_store: Arc::new(Mutex::new(StandaloneServer::new(path, addr)?)),
-            remote_replicas: Arc::new(Mutex::new(clients)),
+            local_store: Arc::new(StandaloneServer::new(path, addr)?),
+            remote_replicas: Arc::new(clients),
         })
     }
 
@@ -66,61 +72,19 @@ where
         info!("{} Replicated get", self.name);
         let req = req.into_inner();
         let key = req.key.clone();
-        let guard = self.local_store.lock().await;
-        let mut response = GetResponse {
-            value: None,
-            timestamp: 0,
+        let guard = &self.local_store;
+        let response = match guard.store.get(key).await.unwrap() {
+            Some(r) => r,
+            None => GetResponse {
+                value: None,
+                timestamp: 0,
+            },
         };
-        match guard.store.get(key.clone()).await.unwrap() {
-            Some(local_result) => {
-                info!(
-                    "{} has value locally, checking timestamps from replicas",
-                    self.name
-                );
-                for r in self.remote_replicas.lock().await.iter_mut() {
-                    match r.get(key.clone()).await.unwrap() {
-                        Some(remote_result) => {
-                            if remote_result.timestamp >= local_result.timestamp {
-                                //r.set(key.clone(), local_result.value.clone().unwrap())
-                                //    .await
-                                //    .unwrap();
-                                response = remote_result.clone();
-                                guard
-                                    .store
-                                    .set(key.clone(), remote_result.value.unwrap())
-                                    .await
-                                    .unwrap();
-                            }
-                        }
-                        None => response = local_result.clone(),
-                    }
-                }
-                info!("{} respondind get {:?}", self.name, response);
-                Ok(tonic::Response::new(response))
-            }
-            None => {
-                info!(
-                    "{} does not have {}, checking remotely",
-                    self.name,
-                    req.key.clone()
-                );
-                for r in self.remote_replicas.lock().await.iter_mut() {
-                    match r.get(key.clone()).await.unwrap() {
-                        Some(remote_result) => {
-                            if let Some(v) = remote_result.clone().value {
-                                if remote_result.timestamp > response.timestamp {
-                                    response = remote_result;
-                                    guard.store.set(req.key.clone(), v).await.unwrap();
-                                }
-                            }
-                        }
-                        None => {}
-                    }
-                }
-                info!("{} respondind get {:?}", self.name, response);
-                Ok(tonic::Response::new(response))
-            }
-        }
+
+        let client_one = &self.remote_replicas[0];
+        let client_two = &self.remote_replicas[1];
+
+        Ok(tonic::Response::new(response))
     }
 
     async fn set(
@@ -129,28 +93,11 @@ where
     ) -> tonic::Result<tonic::Response<Acknowledgement>, tonic::Status> {
         info!("{} Replicated set", self.name);
         let req = req.into_inner();
-        let local = self.local_store.lock().await;
-        match local.store.get(req.key.clone()).await.unwrap() {
-            Some(local_response) => {
-                if req.timestamp > local_response.timestamp {
-                    local
-                        .store
-                        .set(req.key.clone(), req.value.clone())
-                        .await
-                        .unwrap();
-                    for r in self.remote_replicas.lock().await.iter_mut() {
-                        r.set(req.key.clone(), req.value.clone()).await.unwrap();
-                    }
-                } else {
-                    if let Some(value) = local_response.value {
-                        for r in self.remote_replicas.lock().await.iter_mut() {
-                            r.set(req.key.clone(), value.clone()).await.unwrap();
-                        }
-                    }
-                }
-            }
-            None => local.store.set(req.key, req.value).await.unwrap(),
-        }
+        self.local_store
+            .store
+            .set(req.key.clone(), req.value.clone())
+            .await
+            .unwrap();
         Ok(tonic::Response::new(Acknowledgement { success: true }))
     }
 
@@ -182,11 +129,17 @@ mod test {
             .unwrap()
     }
 
+    async fn client_three() -> RemoteNodeClient {
+        RemoteNodeClient::new("127.0.0.1:6002".to_string())
+            .await
+            .unwrap()
+    }
+
     #[tokio::test]
     async fn general_replication() {
         // Binds to 6000 and connects to 6001 for replication
         let node_one = ReplicatedServer::new(
-            vec![client_two().await],
+            [client_two().await, client_three().await],
             "node_one".to_string(),
             TempDir::new().unwrap().into_path(),
             "127.0.0.1:6000".parse().unwrap(),
@@ -194,15 +147,25 @@ mod test {
         .unwrap();
         // Binds to 6001 and connects to 6000 for replication
         let node_two = ReplicatedServer::new(
-            vec![client_one().await],
+            [client_one().await, client_three().await],
             "node_two".to_string(),
             TempDir::new().unwrap().into_path(),
             "127.0.0.1:6001".parse().unwrap(),
         )
         .unwrap();
 
+        // Binds to 6001 and connects to 6000 for replication
+        let node_three = ReplicatedServer::new(
+            [client_one().await, client_two().await],
+            "node_two".to_string(),
+            TempDir::new().unwrap().into_path(),
+            "127.0.0.1:6002".parse().unwrap(),
+        )
+        .unwrap();
+
         tokio::spawn(async move { node_one.run().await.unwrap() });
         tokio::spawn(async move { node_two.run().await.unwrap() });
+        tokio::spawn(async move { node_three.run().await.unwrap() });
 
         // Let the nodes startup
         thread::sleep(Duration::from_millis(1500));
