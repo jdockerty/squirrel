abc456c9ecfbb32f901f60eaea309c6ac05da383
Jack Dockerty <jdockerty19@gmail.com>
2024-04-15 17:26:15 +0100

    basic test for replication mode cli

--- src/replication/mod.rs
+++ src/replication/mod.rs
@@ -1,7 +1,8 @@
-//! Replication is achieved through a simplistic replicator/follower model.
+//! Replication is achieved through a simplistic leader/follower model.
 //!
-//! The [`ReplicatedServer`] is the designated replicator and will use its internally
-//! configured [`RemoteNodeClient`]'s to update/retrieve values from remote stores.
+//! The [`ReplicatedServer`] is the designated leader and will use its internally
+//! configured [`RemoteNodeClient`]'s to update and retrieve values from remote
+//! stores.
 
 mod server;
 
--- tests/replicated_store.rs
+++ tests/replicated_store.rs
@@ -1,1 +1,11 @@
+use assert_cmd::prelude::*;
+use std::process::Command;
+use tempfile::TempDir;
 
+#[test]
+fn replicated_setup() {
+    let temp_dir = TempDir::new().unwrap();
+    let mut cmd = Command::cargo_bin("sqrl-server").unwrap();
+    cmd.args(["--replication-mode",  "leader", "--addr", "127.0.0.1:7000"])
+        .current_dir(&temp_dir).assert().success();
+}
