8565b789352bc9306b299673c27b7e54d68ce0ca
Jack Dockerty <jdockerty19@gmail.com>
2024-04-15 17:12:37 +0100

    docstring for replication crate

--- src/replication/mod.rs
+++ src/replication/mod.rs
@@ -1,3 +1,8 @@
+//! Replication is achieved through a simplistic replicator/follower model.
+//!
+//! The [`ReplicatedServer`] is the designated replicator and will use its internally
+//! configured [`RemoteNodeClient`]'s to update/retrieve values from remote stores.
+
 mod server;
 
 pub use server::ReplicatedServer;
